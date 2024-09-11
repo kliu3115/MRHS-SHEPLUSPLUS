3c96072e45fa32dc5ff20324786a7cc5bfc12e46
kliu3115 <kliu3115@gmail.com>
2024-09-10 22:55:47 -0400

    added vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import PeerMentoring from './PeerMentoring'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
-
   return (
     <Router>
       <div className = 'App'>
