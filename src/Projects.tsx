1d305c7be8f6cd16c1eb052b5e992fedf032a1db
kliu3115 <kliu3115@gmail.com>
2024-10-11 23:06:53 -0400

    updated projects tab w/ latest dates

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -2,12 +2,13 @@ const Projects = () => {
     return (
         <div className = "projects">
             <h1> Upcoming Projects </h1>
-            <button className="projectButton"> Build Your Own Website Workshop </button>
-            <button className="projectButton"> Women in Tech Week Presentation</button>
-            <button className="projectButton"> Middle School Coding Crash Course </button>
-            <button className="projectButton"> Alumni College/Career Panel </button>
+            <h2> Book Drive: Donation Box Decoration (10/23 after school room 354) & Actual Drive (10/28-11/26) </h2>
+            <h2> Build Your Own Website Workshop </h2>
+            <h2> Middle School Coding Crash Course </h2>
+            <h2> Alumni College/Career Panel </h2>
+            <h2> <a style={{color: "#008080"}}href="https://forms.gle/DQpFRTdm1QLpTtDaA"> Let us know if you have any other suggestions! </a></h2>
             <br /> <br />
-            <h1> Projects/Initiatives </h1>
+            <h1> Past Projects/Initiatives </h1>
             <a href="/projects/cte-video"><button className="projectButton"> HCPSS CTE Course Introduction Video </button></a>
             <a href="projects/black-history-month"><button className="projectButton"> Black History Month Women In STEM </button></a>
             <a href="/projects/mvms-stem-fair"><button className="projectButton"> Mount View Middle School STEM Fair </button></a>
@@ -17,7 +18,6 @@ const Projects = () => {
             <h1> Workshops/Hackathons </h1>
             <a href="/projects/c++-workshop"><button className="projectButton"> C++ Workshop </button></a>
             <a href="projects/java-workshop"><button className="projectButton"> Java Workshop </button></a>
-            <button className="projectButton"> Hour of Code Mini-Hackathon </button>
         </div>    
     )
 }
